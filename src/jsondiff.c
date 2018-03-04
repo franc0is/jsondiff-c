348fbff5c77de948a8c930e92b10c8d383981d1c
François Baldassari <francois.baldassari@oculus.com>
2018-03-04 12:29:22 -0600

    Object diff, integer diff done, string, list TODO

--- src/jsondiff.c
+++ src/jsondiff.c
@@ -5,31 +5,79 @@
 static json_t *
 prv_replace_op(json_t *new)
 {
-    return NULL;
+    return json_pack("{s:s, s:O}", "o", "r", "v", new);
+}
+
+static json_t *
+prv_delete_op(void)
+{
+    return json_pack("{s:s}", "o", "-");
+}
+
+static json_t *
+prv_add_op(json_t *new)
+{
+    return json_pack("{s:s, s:O}", "o", "+", "v", new);
+}
+
+static json_t *
+prv_modify_op(json_t *changes)
+{
+    return json_pack("{s:s, s:O}", "o", "M", "v", changes);
 }
 
 static json_t *
 prv_integer_diff(json_t *a, json_t *b, int flags)
 {
-    return NULL;
+    assert(json_is_integer(a) && json_is_integer(b));
+
+    int delta = json_integer_value(b) - json_integer_value(a);
+    return json_pack("{s:s, s:i}", "o", "I", "v", delta);
 }
 
 static json_t *
 prv_string_diff(json_t *a, json_t *b, int flags)
 {
-    return NULL;
+    // FIXME
+    return prv_replace_op(b);
 }
 
 static json_t *
 prv_object_diff(json_t *a, json_t *b, int flags)
 {
-    return NULL;
+    assert(json_is_object(a) && json_is_object(b));
+
+    // copy b so we can edit it in place
+    json_t *b_copy = json_copy(b);
+
+    // remove or modify all objects in A but different in B
+    json_t *changes = json_object();
+    const char *key;
+    json_t *value;
+    json_t *diff = NULL;
+    json_object_foreach(a, key, value) {
+        json_t *b_value = json_object_get(b, key);
+        if (!b_value) {
+            json_object_set(changes, key, prv_delete_op());
+        } else if ((diff = jsondiff_compare(value, b_value, flags))) {
+            json_object_set(changes, key, diff);
+            json_object_del(b_copy, key);
+        }
+    }
+
+    // add all objects left in B
+    json_object_foreach(b_copy, key, value) {
+        json_object_set(changes, key, prv_add_op(value));
+    }
+
+    return prv_modify_op(changes);
 }
 
 static json_t *
 prv_array_diff(json_t *a, json_t *b, int flags)
 {
-    return NULL;
+    // FIXME
+    return prv_replace_op(b);
 }
 
 json_t *
@@ -39,7 +87,7 @@ jsondiff_compare(json_t *a, json_t *b, int flags)
     int b_type = json_typeof(b);
 
     if (json_equal(a, b)) {
-        return json_object(); // empty object, XXX should it be null?
+        return NULL;
     }
 
     if (a_type != b_type &&
