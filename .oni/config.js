3633c9cb997b34dfc862c99ef1cc780bd2d240fa
François Baldassari <francois.baldassari@oculus.com>
2018-03-25 22:52:25 -0700

    Add oni config

---
+++ .oni/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "language.c.languageServer.arguments": ["-compile-commands-dir", Oni.workspace._activeWorkspace + "/build"],
+    "language.cpp.languageServer.arguments": ["-compile-commands-dir", Oni.workspace._activeWorkspace + "/build"],
+}
