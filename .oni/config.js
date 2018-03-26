ce453175aa232f2247ebc74bad6137e9e54486ea
François Baldassari <francois.baldassari@oculus.com>
2018-03-26 09:24:52 -0700

    Add travis build

--- .oni/config.js
+++ .oni/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "language.c.languageServer.arguments": ["-compile-commands-dir", Oni.workspace._activeWorkspace + "/build"],
-    "language.cpp.languageServer.arguments": ["-compile-commands-dir", Oni.workspace._activeWorkspace + "/build"],
+    "language.c.languageServer.arguments": ["-compile-commands-dir", Oni.workspace.activeWorkspace + "/build"],
+    "language.cpp.languageServer.arguments": ["-compile-commands-dir", Oni.workspace.activeWorkspace + "/build"],
 }
